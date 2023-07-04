7436a254d591d0560fcf0111e946f4fcdf29233c
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-07-04 15:02:02 +0300

    completed assignment

---
+++ server/db/seeds.rb
@@ -0,0 +1,26 @@
+require 'faker'
+
+# Create 10 restaurants
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address
+  )
+end
+
+# Create 20 pizza
+20.times do
+  Pizza.create!(
+    name: Faker::Food.dish,
+    ingredients: Faker::Food.ingredient
+  )
+end
+
+# Create 50 restaurant_pizza
+50.times do
+  RestaurantPizza.create!(
+    price: rand(1...13),
+    restaurant: Restaurant.order("RANDOM()").first,
+    pizza: Pizza.order("RANDOM()").first
+  )
+end
